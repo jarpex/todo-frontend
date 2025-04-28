4e62fc26d5fa3d586abad86db8ae99b33824c18f
Default Jarpex <default@jarpex.ru>
2025-04-29 01:29:10 +0300

    chore: moved API to https://api-todo.jarpex.com

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.1.160:30082/api/v1';
+export const API_BASE_URL = 'https://api-todo.jarpex.com/api/v1';
 import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1';
